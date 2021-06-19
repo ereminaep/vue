1f8e27b9b7c9c2f92bf883682fae62b60f95357e
ereminaep <57357282+ereminaep@users.noreply.github.com>
2021-06-19 09:59:01 +0300

    week1: стили для скиллов

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,44 @@
-console.log('this is the skills module');
+let el = document.querySelectorAll('.skill');
+for (let i = 0; i < el.length; i++) {
+    var options = {
+        percent: el[i].getAttribute('data-percent') || 25,
+        size: el[i].getAttribute('data-size') || 220,
+        text: el[i].getAttribute('data-text') || 220,
+        lineWidth: el[i].getAttribute('data-line') || 15,
+        rotate: el[i].getAttribute('data-rotate') || 0
+    }
+
+    var canvas = document.createElement('canvas');
+    var span = document.createElement('span');
+    span.classList.add('skill__text');
+    span.textContent = options.text;
+
+    if (typeof(G_vmlCanvasManager) !== 'undefined') {
+        G_vmlCanvasManager.initElement(canvas);
+    }
+
+    var ctx = canvas.getContext('2d');
+    canvas.width = canvas.height = options.size;
+
+    el[i].appendChild(span);
+    el[i].appendChild(canvas);
+
+    ctx.translate(options.size / 2, options.size / 2);
+    ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI);
+
+    //imd = ctx.getImageData(0, 0, 240, 240);
+    var radius = (options.size - options.lineWidth) / 2;
+
+    var drawCircle = function(color, lineWidth, percent) {
+        percent = Math.min(Math.max(0, percent || 1), 1);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth
+        ctx.stroke();
+    };
+
+    drawCircle('rgba(255, 255, 255, 1)', 1, 100 / 100);
+    drawCircle('#dc9322', options.lineWidth, options.percent / 100);
+
+}
