c957917c68a1b144a5d3d64a7cf6226e6329fb4e
ereminaep <57357282+ereminaep@users.noreply.github.com>
2021-06-21 17:12:17 +0300

    week2: добавлено меню на адаптиве

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,8 +2,10 @@
     document.querySelector('.hamburger').addEventListener('click', function() {
         let body = document.querySelector('body');
         let header = document.querySelector('.header');
+        let main = document.querySelector('.main');
         this.classList.toggle('hamburger__active');
         body.classList.toggle('not-scroll');
         header.classList.toggle('header__popup');
+        main.classList.toggle('main--popup');
     });
 })()
