74664ed59464bf1e4a838c6bd64e2fc7cae778b9
ereminaep <57357282+ereminaep@users.noreply.github.com>
2021-06-25 22:05:19 +0300

    week2: добавила хэширование урлов картинок

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -7,28 +7,26 @@ let reviews = Vue.component('reviews', {
 
 let review = Vue.component('review', {
     props: ['text', 'name', 'work', 'image'],
-    template: "#review"
+    template: "#review",
+    methods: {
+        requareImage: function(img) {
+            let requiredImg = require(`../images/content/${img}`);
+            return requiredImg.default;
+        }
+    },
+    mounted: function() {
+        let beforeImg = this.$el.querySelector('img').getAttribute('data-src');
+        let afterImg = this.requareImage(beforeImg);
+        this.$el.querySelector('img').setAttribute('src', afterImg);
+    }
 })
 
 const vueModel = new Vue({
     el: '#reviews__widget',
     template: '#reviews__components',
-    components: { reviews, review },
     data() {
         return {
             reviews_data: require('./../data/reviews.json')
         }
-    },
-    methods: {
-        /*requareImagesToArray(data) {
-            return data.map(item => {
-                const requiredImage = requare(`../images/content/${item.image}`);
-                item.image = requiredImage;
-                return item
-            })
-        }*/
-    },
-    created() {
-        //this.reviews_data = this.requareImagesToArray();
     }
 });
