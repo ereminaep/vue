f891d30c7d1518730abe1971ab5ed39ccf1922e3
ereminaep <57357282+ereminaep@users.noreply.github.com>
2021-06-26 18:35:13 +0300

    week2: данные в блок работ настроены из json

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -3,4 +3,22 @@ import Vue from 'vue';
 const vueModel = new Vue({
     el: '#works__widget',
     template: '#works__components',
+    data() {
+        return {
+            works_data: require('./../data/works.json'),
+        }
+    },
+    methods: {
+        requareImage: function() {
+            this.works_data.map(
+                function(item) {
+                    let requiredImg = require(`../images/content/${item.image}`);
+                    item.image = requiredImg.default;
+                }
+            )
+        }
+    },
+    created: function() {
+        this.requareImage();
+    }
 });
