909b3a56e8c8bd3784242e6bf8da2a093c20509d
ereminaep <57357282+ereminaep@users.noreply.github.com>
2021-06-26 08:20:20 +0300

    переделала реализацию отзывов

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,26 +1,5 @@
 import Vue from 'vue';
 
-let reviews = Vue.component('reviews', {
-    props: ['items'],
-    template: "#reviews-list"
-})
-
-let review = Vue.component('review', {
-    props: ['text', 'name', 'work', 'image'],
-    template: "#review",
-    methods: {
-        requareImage: function(img) {
-            let requiredImg = require(`../images/content/${img}`);
-            return requiredImg.default;
-        }
-    },
-    mounted: function() {
-        let beforeImg = this.$el.querySelector('img').getAttribute('data-src');
-        let afterImg = this.requareImage(beforeImg);
-        this.$el.querySelector('img').setAttribute('src', afterImg);
-    }
-})
-
 const vueModel = new Vue({
     el: '#reviews__widget',
     template: '#reviews__components',
@@ -28,5 +7,19 @@ const vueModel = new Vue({
         return {
             reviews_data: require('./../data/reviews.json')
         }
+    },
+    methods: {
+        requareImage: function() {
+            this.reviews_data.map(
+                function(item) {
+                    let requiredImg = require(`../images/content/${item.image}`);
+                    item.image = requiredImg.default;
+                }
+            )
+        }
+    },
+    created: function() {
+        this.requareImage();
+        console.log(this.reviews_data);
     }
 });
